eb893ce9cfe4f805f32b3fe924ca778237f4d454
Alexander Zamaschikov <49248483+azamaschikov@users.noreply.github.com>
2020-05-13 20:22:36 +0800

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -2,23 +2,20 @@
 
 import os
 import sys
-import time
 import requests
 
-current_time = time.strftime('%a %b %d %T %Z')
-
 def send_message(text):
-    chat_id = 'your_chat_id'
     bot_id = 'your_bot_id'
+    chat_id = 'your_chat_id'
 
     response = requests.post('https://api.telegram.org/bot' + bot_id + '/sendMessage', data={'chat_id':chat_id, 'text':text})
     response = response.json()
 
 if sys.argv[1] == '--cpu':
-    send_message('CPU is is exceeded')
+    send_message('CPU is exceeded')
 
 if sys.argv[1] == '--memory':
-    send_message('RAM is is exceeded')
+    send_message('RAM is exceeded')
 
 if sys.argv[1] == '--io-disk':
-    send_message('IO-disks is is exceeded')
+    send_message('IO-disks is exceeded')
